3656fae984db892dd1f18efa463cec37e46ed469
pauloalchemist <paulolinsdev@gmail.com>
2021-02-25 21:39:20 -0300

    adcionado transação para criar usário e conta com Ecto.Multi

--- nicepay/lib/nicepay/users/create.ex
+++ nicepay/lib/nicepay/users/create.ex
@@ -1,9 +1,32 @@
 defmodule Nicepay.Users.Create do
-  alias Nicepay.{Repo, User}
+
+  alias Ecto.Multi
+  alias Nicepay.{Account, Repo, User}
 
   def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      insert_account(repo, user)
+    end)
+    |> run_transaction()
+  end
+
+  defp insert_account(repo, user) do
+    user.id
+    |> account_changeset()
+    |> repo.insert()
+  end
+
+  defp account_changeset(user_id) do
+    params = %{user_id: user_id, balance: "0.00"}
+    Account.changeset(params)
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _change} -> {:error, reason}
+      {:ok, %{create_account: account}} -> IO.inspect(account)
+    end
   end
 end
