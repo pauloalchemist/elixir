51fea6643079cbcbc3a0f007d24b37e19b728be2
pauloalchemist <paulolinsdev@gmail.com>
2021-02-26 14:01:00 -0300

    refactored deposit module

--- nicepay/lib/nicepay/accounts/deposit.ex
+++ nicepay/lib/nicepay/accounts/deposit.ex
@@ -9,6 +9,7 @@ defmodule Nicepay.Accounts.Deposit do
     |> Multi.run(:update_balance, fn repo, %{account: account} ->
       update_balance(repo, account, value)
     end)
+    |> run_transaction()
   end
 
   defp get_account(repo, id) do
@@ -21,7 +22,7 @@ defmodule Nicepay.Accounts.Deposit do
   defp update_balance(repo, account, value) do
     account
     |> sum_values(value)
-    |> update_account(repo)
+    |> update_account(repo, account)
   end
 
   defp sum_values(%Account{balance: balance}, value) do
@@ -32,4 +33,21 @@ defmodule Nicepay.Accounts.Deposit do
 
   defp handle_cast({:ok, value}, balance), do: Decimal.add(value, balance)
   defp handle_cast(:error, _balance), do: {:error, "Invalid deposit value!"}
+
+  defp update_account({:error, _reason} = error, _repo, _account), do: error
+
+  defp update_account(value, repo, account) do
+    params = %{balance: value}
+
+    account
+    |> Account.changeset(params)
+    |> repo.update()
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _change} -> {:error, reason}
+      {:ok, %{update_balance: account}} -> {:ok, account}
+    end
+  end
 end
